daefd461b609547dcf34abaa9ea01fadd8055d34
Hariharan Mohan <haranhari350@gmail.com>
2024-11-25 13:51:29 -0600

    Created browser routs via: createRoutesFromElements and createBrowserRouter; Tradiational way of creating browser routs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Home />} />
+    <Route path="/products" element={<Products />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routeDefinitions);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
